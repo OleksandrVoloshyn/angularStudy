a4d4fecac897162a05cac5ddee334cfc1fbbecac
OleksandrVoloshyn <oleksandr.vvn@gmail.com>
2022-05-27 20:50:40 +0300

    pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {DataService} from "./services";
-import {IUser} from "./interfaces";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  user: IUser;
+export class AppComponent {
+  title = 'sep'
 
-  constructor(private dataService: DataService) {
-  }
-
-  ngOnInit(): void {
-    this.dataService.storage.subscribe(value => this.user = value)
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule} from "@angular/common/http";
 
 import {AppComponent} from './app.component';
-import {UsersComponent} from './components/users/users.component';
-import {UserComponent} from './components/user/user.component';
+import { CustomPipe } from './services/pipes/custom.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
+    CustomPipe,
+
   ],
   imports: [
     BrowserModule,
